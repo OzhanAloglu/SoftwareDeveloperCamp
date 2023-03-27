fc0856308f14a35454328803c49975d1b6ad7773
Özhan <ozhanaloglu1@gmail.com>
2023-03-27 17:25:56 +0300

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using Business.Concrete;
+
+ProductManager productManager = new ProductManager();
+
+Console.WriteLine("Hello, World!");
