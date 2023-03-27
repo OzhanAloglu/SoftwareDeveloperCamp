bf3c0b9ed0244906c2d74396ccea8f3740a54741
Özhan <ozhanaloglu1@gmail.com>
2023-03-27 17:29:03 +0300

    Mimari tasarlanıyor

--- ConsoleApp1/Program.cs
+++
@@ -1,8 +0,0 @@
-﻿// See https://aka.ms/new-console-template for more information
-
-
-using Business.Concrete;
-
-ProductManager productManager = new ProductManager();
-
-Console.WriteLine("Hello, World!");
