25ce78c9abecb0d7b5882ad3ed71cbab01b39879
Özhan <ozhanaloglu1@gmail.com>
2023-03-28 17:55:08 +0300

    Veritabanı bağlantısı ve Ef işlemleri

---
+++ UI/Program.cs
@@ -0,0 +1,22 @@
+﻿using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
+using Entities.Concrete;
+
+class Program
+{
+    //SOLID
+    //Open-Closed Principle------->>Yeni bir kod eklediğin zaman varolan kodu değiştirmeme.
+
+
+
+    static void Main(string[] args)
+    {
+        ProductManager productManager=new ProductManager(new EfProductDal());
+
+        foreach (var product in productManager.GetByUnitPrice(10, 400))
+        {
+            Console.WriteLine(product.ProductName);
+
+        }
+    }
+}
