ac4c09c3c48d3c0f6049792278437461ecd06156
Özhan <ozhanaloglu1@gmail.com>
2023-03-31 03:36:22 +0300

    .

--- Business/Abstract/IProductService.cs
+++ Business/Abstract/IProductService.cs
@@ -1,4 +1,5 @@
-﻿using Entities.Concrete;
+﻿using Core.Utilities.Results;
+using Entities.Concrete;
 using Entities.DTOs;
 using System;
 using System.Collections.Generic;
@@ -18,6 +19,8 @@ namespace Business.Abstract
 
         List<ProductDetailDto> GetProductDetails();
 
+        IResult Add(Product product);
+        Product GetById(int productId);
 
     }
 }
--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -1,4 +1,6 @@
 ﻿using Business.Abstract;
+using Business.Constants;
+using Core.Utilities.Results;
 using DataAccess.Abstract;
 using DataAccess.Concrete.InMemory;
 using Entities.Concrete;
@@ -21,6 +23,21 @@ namespace Business.Concrete
             _productDal = productDal;
         }
 
+        public IResult Add(Product product)
+        {
+            //business codes
+            if (product.ProductName.Length<2)
+            {
+                //magic string
+                return new ErrorResult(Messages.ProductNameInValid);
+            }
+
+           _productDal.Add(product);
+
+            return new SuccessResult(Messages.ProductAdded);
+
+        }
+
         public List<Product> GetAll()
         {
             //İş Kodları
@@ -32,6 +49,11 @@ namespace Business.Concrete
         public List<Product> GetAllByCategoryId(int id)
         {
             return _productDal.GetAll(p => p.CategoryId == id);
+        }
+
+        public Product GetById(int productId)
+        {
+            return _productDal.Get(x=>x.ProductId == productId);
         }
 
         public List<Product> GetByUnitPrice(decimal min, decimal max)
